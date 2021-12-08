463ebe5237c319c6ba4b5a16c841909d0d83e874
fazeelhaider <fazeel.haider78@gmail.com>
2021-12-08 23:44:27 +0500

    m

--- Functions/Product.js
+++ Functions/Product.js
@@ -290,7 +290,7 @@ function UploadSingleFile(File, __dirname){
             const {
                 WillLastId
               } = result;
-            const Path = `${__dirname}/Uploads/${WillLastId}-${File.name}`;
+            const Path = `../Uploads/${WillLastId}-${File.name}`;
             File.mv(Path, function(err) {
                 if (!err){
                     res(`Uploads/${WillLastId}-${File.name}`);
